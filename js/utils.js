2c591cd36f586e71a8c1798ce2cbbfaa341e7618
Lucas Rodrigues Moura <lucas.rodrigues20@outlook.com>
2019-05-20 22:19:49 -0300

    add all

---
+++ js/utils.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+    // Get the current year for the copyright
+    $('#year').text(new Date().getFullYear());
+
+    $('#main-nav a').on('click', function(event) {
+        if (this.hash !== '') {
+            event.preventDefault();
+
+            const hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
+
+});
